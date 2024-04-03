db6bf81615579d0b919920fa1067273f12799209
oscar <oscar.cardenas.araya@institutocomercialosorno.cl>
2024-04-03 12:35:28 -0400

    Primer commit

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const counter = document.getElementById('like');
+    const incrementButton = document.getElementById('contador');
+  
+    let count = 0;
+  
+    contador.addEventListener('click', function () {
+      count++;
+      like.textContent = count;
+    });
+  });
+  
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const counter = document.getElementById('like2');
+    const incrementButton = document.getElementById('contador2');
+  
+    let count = 0;
+  
+    contador2.addEventListener('click', function () {
+      count++;
+      like2.textContent = count;
+    });
+  });
+
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const counter = document.getElementById('like3');
+    const incrementButton = document.getElementById('contador3');
+  
+    let count = 0;
+  
+    contador3.addEventListener('click', function () {
+      count++;
+      like3.textContent = count;
+    });
+  });
+
